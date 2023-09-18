70fe4e8b68c2bdcb0da8f01d007d806c086a3d5b
Joseph C <josephalexcobian@gmail.com>
2023-09-17 18:33:45 -0700

    Prevent repainting a grid if it is rehovered

--- etchaSketch.js
+++ etchaSketch.js
@@ -1,4 +1,18 @@
+
 const gridContainer = document.querySelector('.gridContainer');
+//Ensure the grid is always a square
+//Requires an eventlistener to run this when window is resized
+/*
+addEventListener('resize', ()=> {
+    console.log('resizing..');
+if (gridContainer.clientWidth > gridContainer.clientHeight) {
+    gridContainer.style.width = `${gridContainer.clientHeight}px`
+}
+else {
+    gridContainer.style.height = `${gridContainer.clientWidth}px`;
+}
+})
+*/
 let gridWidth = 16;
 let gridHeight = 16;
 
@@ -15,8 +29,13 @@ for (let i = 0; i < gridHeight; i++) {
 
 function paintCell (e, color) {
     const cell = e.target;
-    if (e.target.className === 'gridCell') {
+    if (cell.className === 'gridCell') {
         cell.style.background = `rgb(${color.red}, ${color.green}, ${color.blue})`;
+        cell.classList.add('painted');
+    }
+    else if (cell.className === 'gridCell painted') {
+        let brightness = cell.style.filter;
+        brightness = Number(brightness.slice(-4, -6))
     }
 }
 function getRandomRGB(){
